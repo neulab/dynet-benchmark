451132f2331fefb6639d40261607a24e6db5fa40
Graham Neubig <neubig@gmail.com>
2016-12-05 10:30:46 -0500

    A few changes

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
 PYTHON=python2.7
+DYNET_MEM=1024
 
 # Run python tests
-$PYTHON dynet-py/bilstm-tagger.py --dynet-mem 1024
-$PYTHON dynet-py/bilstm-tagger-withchar.py --dynet-mem 1024
+$PYTHON dynet-py/bilstm-tagger.py --dynet-mem $DYNET_MEM
+$PYTHON dynet-py/bilstm-tagger-withchar.py --dynet-mem $DYNET_MEM
+$PYTHON dynet-py/rnnlm.py --dynet-mem $DYNET_MEM
+$PYTHON dynet-py/rnnlm-batch.py --dynet-mem $DYNET_MEM
+$PYTHON dynet-py/recnn.py --dynet-mem $DYNET_MEM
+
+# Run C++ tests
+dynet-cpp/bilstm-tagger --dynet-mem $DYNET_MEM
+dynet-cpp/bilstm-tagger-withchar --dynet-mem $DYNET_MEM
+dynet-cpp/rnnlm --dynet-mem $DYNET_MEM
+dynet-cpp/rnnlm-batch --dynet-mem $DYNET_MEM
+dynet-cpp/recnn --dynet-mem $DYNET_MEM
