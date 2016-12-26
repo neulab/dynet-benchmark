455e7fba89b4af5e8218ea65bb4b6d6ee60b9eb6
Chaitanya Malaviya <chaitanyamalaviya@gmail.com>
2016-12-26 20:54:57 +0530

    fixed bug for mb size=1

--- tensorflow/rnnlm-batch.py
+++ tensorflow/rnnlm-batch.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import time
 start = time.time()
 
@@ -83,7 +84,7 @@ with tf.device(cpu_or_gpu):
   x_input = tf.placeholder(tf.int32, [None, None], name="x_input")
   x_lens = tf.placeholder(tf.int32, [None], name='x_lens')
 
-  x_embs = tf.squeeze(tf.nn.embedding_lookup(WORDS_LOOKUP, x_input))
+  x_embs = tf.squeeze(tf.nn.embedding_lookup(WORDS_LOOKUP, x_input), axis=2)
   x_embs.set_shape([None, None, args.EMBED_SIZE])
   cell_out = tf.nn.rnn_cell.OutputProjectionWrapper(cell, nwords)
   outputs, _ = tf.nn.dynamic_rnn(cell_out, x_embs, sequence_length=x_lens, dtype=tf.float32)
@@ -92,22 +93,20 @@ with tf.device(cpu_or_gpu):
   loss = tf.reduce_mean(losses)
   optimizer = tf.train.AdamOptimizer().minimize(loss)
 
-  # print >>sys.stderr, 'Graph created.' 
   print('Graph created.' , file=sys.stderr)
 
 sess = tf.InteractiveSession(config=tf.ConfigProto(intra_op_parallelism_threads=1))
 tf.global_variables_initializer().run()
 print('Session initialized.' , file=sys.stderr)
-# print >>sys.stderr, 'Session initialized.' 
 train_losses = [] 
-print ("startup time: %r" % (time.time() - start))
+print("startup time: %r" % (time.time() - start))
 start = time.time()
 i = all_time = dev_time = all_tagged = train_words = 0
 for ITER in range(10):
   random.shuffle(train_order)
   for i,sid in enumerate(train_order, start=1):
     if i % int(500/args.MB_SIZE) == 0:
-      print "Updates so far:", (i-1), "Loss:" , sum(train_losses) / train_words
+      print("Updates so far:", (i-1), "Loss:" , sum(train_losses) / train_words)
       all_tagged += train_words
       train_losses = []
       train_words = 0
@@ -117,7 +116,7 @@ for ITER in range(10):
       test_losses = []
       test_words = 0
       all_time += time.time() - start
-      print "Testing on dev set..."
+      print("Testing on dev set...")
 
       for tid in test_order:
         t_examples = test[tid:tid+args.MB_SIZE]
@@ -130,7 +129,7 @@ for ITER in range(10):
       nll = sum(test_losses) / test_words
       dev_time += time.time() - dev_start 
       train_time = time.time() - start - dev_time
-      print >>sys.stderr, 'nll=%.4f, ppl=%.4f, time=%.4f, words_per_sec=%.4f' % (nll, math.exp(nll), train_time, all_tagged/train_time)
+      print('nll=%.4f, ppl=%.4f, time=%.4f, words_per_sec=%.4f' % (nll, math.exp(nll), train_time, all_tagged/train_time), file=sys.stderr)
       if all_time > args.TIMEOUT:
         sys.exit(0)
 
