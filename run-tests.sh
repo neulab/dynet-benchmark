d024db5f5b7709fc8190ddc3e37834f0470c7e8a
Daniel Hershcovich <danielh@cs.huji.ac.il>
2017-10-18 15:58:11 +0300

    Split tasks to separate jobs; compile on separate stage

--- run-tests.sh
+++ run-tests.sh
@@ -49,62 +49,74 @@ runcmd() {
   fi
 }
 
-for trial in 1 2 3; do
+NUM_TRIALS=${NUM_TRIALS:-3}
 
-  # Run rnnlm-batch
-  for embsize in 128; do
-    hidsize=$(($embsize*2))
-    for mbsize in 64 16 04 01; do
-      for f in dynet-cpp dynet-py chainer theano tensorflow; do
-        if [[ $f == "dynet-cpp" ]]; then
-          runcmd $f rnnlm-seq "$mbsize $embsize $hidsize 0" $f-ms$mbsize-es$embsize-hs$hidsize-sp0-t$trial
-        fi
-        runcmd $f rnnlm-batch "$mbsize $embsize $hidsize 0" $f-ms$mbsize-es$embsize-hs$hidsize-sp0-t$trial
+for trial in `seq $NUM_TRIALS`; do
+
+  if [[ -z "$TASK" || "$TASK" == "rnnlm-batch" ]]; then
+    # Run rnnlm-batch
+    for embsize in 128; do
+      hidsize=$(($embsize*2))
+      for mbsize in 64 16 04 01; do
+        for f in dynet-cpp dynet-py chainer theano tensorflow; do
+          if [[ $f == "dynet-cpp" ]]; then
+            runcmd $f rnnlm-seq "$mbsize $embsize $hidsize 0" $f-ms$mbsize-es$embsize-hs$hidsize-sp0-t$trial
+          fi
+          runcmd $f rnnlm-batch "$mbsize $embsize $hidsize 0" $f-ms$mbsize-es$embsize-hs$hidsize-sp0-t$trial
+        done
       done
     done
-  done
+  fi
 
-  # run sparse rnnlm-batch on a subset
-  for embsize in 128; do
-    hidsize=$(($embsize*2))
-    for mbsize in 16 01; do
-      for f in dynet-cpp dynet-py; do
-        runcmd $f rnnlm-batch "$mbsize $embsize $hidsize 1" $f-ms$mbsize-es$embsize-hs$hidsize-sp1-t$trial
+  if [[ -z "$TASK" || "$TASK" == "sparse-rnnlm-batch" ]]; then
+    # run sparse rnnlm-batch on a subset
+    for embsize in 128; do
+      hidsize=$(($embsize*2))
+      for mbsize in 16 01; do
+        for f in dynet-cpp dynet-py; do
+          runcmd $f rnnlm-batch "$mbsize $embsize $hidsize 1" $f-ms$mbsize-es$embsize-hs$hidsize-sp1-t$trial
+        done
       done
     done
-  done
+  fi
 
-  # Run bilstm-tagger
-  wembsize=128
-  hidsize=50
-  mlpsize=32
-  for f in dynet-cpp dynet-py chainer theano tensorflow; do
-    runcmd $f bilstm-tagger "$wembsize $hidsize $mlpsize 0" $f-ws$wembsize-hs$hidsize-mlps$mlpsize-su0-t$trial
-    if [[ $f == dynet* ]]; then
-      runcmd $f bilstm-tagger "$wembsize $hidsize $mlpsize 1" $f-ws$wembsize-hs$hidsize-mlps$mlpsize-su1-t$trial
-    fi
-  done
+  if [[ -z "$TASK" || "$TASK" == "bilstm-tagger" ]]; then
+    # Run bilstm-tagger
+    wembsize=128
+    hidsize=50
+    mlpsize=32
+    for f in dynet-cpp dynet-py chainer theano tensorflow; do
+      runcmd $f bilstm-tagger "$wembsize $hidsize $mlpsize 0" $f-ws$wembsize-hs$hidsize-mlps$mlpsize-su0-t$trial
+      if [[ $f == dynet* ]]; then
+        runcmd $f bilstm-tagger "$wembsize $hidsize $mlpsize 1" $f-ws$wembsize-hs$hidsize-mlps$mlpsize-su1-t$trial
+      fi
+    done
+  fi
 
-  # Run bilstm-tagger-withchar
-  cembsize=20
-  wembsize=128
-  hidsize=50
-  mlpsize=32
-  for f in dynet-cpp dynet-py theano chainer; do
-    runcmd $f bilstm-tagger-withchar "$cembsize $wembsize $hidsize $mlpsize 0" $f-cs$cembsize-ws$wembsize-hs$hidsize-mlps$mlpsize-su0-t$trial
-    if [[ $f == dynet* ]]; then
-      runcmd $f bilstm-tagger-withchar "$cembsize $wembsize $hidsize $mlpsize 1" $f-cs$cembsize-ws$wembsize-hs$hidsize-mlps$mlpsize-su1-t$trial
-    fi
-  done
+  if [[ -z "$TASK" || "$TASK" == "bilstm-tagger-withchar" ]]; then
+    # Run bilstm-tagger-withchar
+    cembsize=20
+    wembsize=128
+    hidsize=50
+    mlpsize=32
+    for f in dynet-cpp dynet-py theano chainer; do
+      runcmd $f bilstm-tagger-withchar "$cembsize $wembsize $hidsize $mlpsize 0" $f-cs$cembsize-ws$wembsize-hs$hidsize-mlps$mlpsize-su0-t$trial
+      if [[ $f == dynet* ]]; then
+        runcmd $f bilstm-tagger-withchar "$cembsize $wembsize $hidsize $mlpsize 1" $f-cs$cembsize-ws$wembsize-hs$hidsize-mlps$mlpsize-su1-t$trial
+      fi
+    done
+  fi
 
-  # Run treenn
-  wembsize=128
-  hidsize=128
-  for f in dynet-cpp dynet-py chainer; do
-    runcmd $f treenn "$wembsize $hidsize 0" $f-ws$wembsize-hs$hidsize-su0-t$trial
-    if [[ $f == dynet* ]]; then
-      runcmd $f treenn "$wembsize $hidsize 1" $f-ws$wembsize-hs$hidsize-su1-t$trial
-    fi
-  done
+  if [[ -z "$TASK" || "$TASK" == "treenn" ]]; then
+    # Run treenn
+    wembsize=128
+    hidsize=128
+    for f in dynet-cpp dynet-py chainer; do
+      runcmd $f treenn "$wembsize $hidsize 0" $f-ws$wembsize-hs$hidsize-su0-t$trial
+      if [[ $f == dynet* ]]; then
+        runcmd $f treenn "$wembsize $hidsize 1" $f-ws$wembsize-hs$hidsize-su1-t$trial
+      fi
+    done
+  fi
 
 done
